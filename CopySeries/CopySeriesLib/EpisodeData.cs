f04b1ba636938ba9f7a2180a537c70cf96ed865a
DJDoena <github@doena-soft.de>
2022-11-05 20:51:41 +0100

    update nuget

--- CopySeries/CopySeriesLib/EpisodeData.cs
+++ CopySeries/CopySeriesLib/EpisodeData.cs
@@ -4,7 +4,7 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
-    using DoenaSoft.MediaInfoHelper;
+    using MediaInfoHelper;
     using ToolBox.Extensions;
 
     public sealed class EpisodeData : IComparable<EpisodeData>
